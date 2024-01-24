8f730c10bebdbb66abcd06a910dd32ada644f487
Chereji Iulia <72073380+912-Chereji-Iulia@users.noreply.github.com>
2024-01-24 14:51:15 +0200

    added statistics reformatted

--- Statistics.py
+++ Statistics.py
@@ -16,5 +16,17 @@ class DatabaseStatistics:
         if not table_df.empty:
             print(f"\n{table_name} Table Statistics:")
             print(table_df.describe(include='all'))
+            describe_stats = table_df.describe(include='all')
+            statistics_dict = {
+                'count': describe_stats.loc['count'].to_dict(),
+                'mean': describe_stats.loc['mean'].to_dict(),
+                'std': describe_stats.loc['std'].to_dict(),
+                'min': describe_stats.loc['min'].to_dict(),
+                '25%': describe_stats.loc['25%'].to_dict(),
+                '50%': describe_stats.loc['50%'].to_dict(),
+                '75%': describe_stats.loc['75%'].to_dict(),
+                'max': describe_stats.loc['max'].to_dict()
+            }
+            return statistics_dict
         else:
             print(f"\n{table_name} Table is empty.")
